2507e2a09d8bde39e1c362a6eeee8dce30213327
segundara <segundara@gmail.com>
2020-06-26 13:47:12 +0300

    errorhandler added to server js

--- src/server.js
+++ src/server.js
@@ -2,6 +2,7 @@ const express = require("express")
 const productsRoutes = require("./routes/products")
 const reviewsRoutes = require("./routes/reviews")
 const listEndpoints = require("express-list-endpoints")
+const {checkRateError} = require("./errorHandler")
 
 const server = express()
 
@@ -13,6 +14,7 @@ server.use(express.json())
 server.use("/products", productsRoutes)
 server.use("/reviews", reviewsRoutes)
 console.log(listEndpoints(server))
+server.use(checkRateError)
 
 server.listen(port, () => {
     console.log(`Server is running on PORT:${port}`)

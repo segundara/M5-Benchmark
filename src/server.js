3fc21a9d0f6434d2c7ad58edb6f9614ab78e2edc
klevinb <klevin.bazaiti98@gmail.com>
2020-06-26 10:19:45 +0200

    listEndpoints module was added

--- src/server.js
+++ src/server.js
@@ -1,12 +1,16 @@
 const express = require("express")
 const productsRoutes = require("../routes/products")
 const reviewsRoutes = require("../routes/reviews")
+const listEndpoints = require("express-list-endpoints")
 
 const server = express()
 
+server.use(express.json())
+
+// Routes
 server.use("/products", productsRoutes)
 server.use("/reviews", reviewsRoutes)
-
+console.log(listEndpoints(server))
 
 server.listen(3001, () => {
     console.log("Server is running on PORT:3001")
